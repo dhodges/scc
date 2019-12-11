6cd0132290b9f1bda8a127cbff5453002d4de8c8
David Hodges <hodges.david@gmail.com>
2019-12-11 11:36:11 +1100

    main: demonstrate example scenarios

--- main.go
+++ main.go
@@ -2,8 +2,58 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/dhodges/scc/checkout"
+	"github.com/dhodges/scc/pricing"
 )
 
+func printCheckout(customer pricing.Customer, co checkout.Checkout) {
+	fmt.Printf("Customer: %s\n", customer)
+	fmt.Print("items:   ")
+	for _, item := range co.Items() {
+		fmt.Printf("`%s`,", item)
+	}
+	fmt.Println()
+	fmt.Printf("Total:   $%.2f\n", co.Total())
+	fmt.Println()
+}
+
+func checkoutAndPrint(customer pricing.Customer, ads []pricing.AdName) {
+	pricingRules, err := pricing.FetchPricingRules(customer)
+	if err != nil {
+		fmt.Printf("error when fetching pricing rules: %v", err)
+	} else {
+		co := checkout.New(pricingRules)
+		for _, ad := range ads {
+			co.Add(ad)
+		}
+		printCheckout(customer, co)
+	}
+}
+
 func main() {
-	fmt.Printf("g'day")
+
+	fmt.Println()
+	fmt.Println("Example scenarios")
+	fmt.Println()
+
+	checkoutAndPrint(pricing.Customer("default"), []pricing.AdName{
+		pricing.AdName("classic"),
+		pricing.AdName("standout"),
+		pricing.AdName("premium"),
+	})
+
+	checkoutAndPrint(pricing.Customer("SecondBite"), []pricing.AdName{
+		pricing.AdName("classic"),
+		pricing.AdName("classic"),
+		pricing.AdName("classic"),
+		pricing.AdName("premium"),
+	})
+
+	checkoutAndPrint(pricing.Customer("Axil Coffee Roasters"), []pricing.AdName{
+		pricing.AdName("standout"),
+		pricing.AdName("standout"),
+		pricing.AdName("standout"),
+		pricing.AdName("premium"),
+	})
 }
