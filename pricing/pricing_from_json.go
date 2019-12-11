3088a63963fa01eb0d0cfdfa317275042b837940
David Hodges <hodges.david@gmail.com>
2019-12-11 11:45:23 +1100

    central json file for pricing rules

--- pricing/pricing_from_json.go
+++ pricing/pricing_from_json.go
@@ -4,13 +4,18 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"path/filepath"
+	"runtime"
 )
 
 // LoadPrices gather all known ad prices, including customer deals
 func loadPricesFromJSON() (Rules, error) {
 	var priceRules Rules
 
-	data, err := ioutil.ReadFile("./pricing_rules.json")
+	_, filename, _, _ := runtime.Caller(0)
+	dir := filepath.Dir(filename)
+	dir = filepath.Dir(dir) //; get parent dir
+	data, err := ioutil.ReadFile(dir + "/pricing_rules.json")
 	if err != nil {
 		fmt.Print(err)
 	} else {
